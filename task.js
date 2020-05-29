35d79624aa790ec0b0fe9b622255783b0cc8534c
Gevorg Kirakosyan <gevorg.kirakosyan@softconstruct.local>
2020-05-30 03:58:59 +0400

    dzer xatr

--- task.js
+++ task.js
@@ -13,9 +13,25 @@ function task14(a, b) {
         return a ** 15;
 }
 task14(1, 1);
+//25
 function task25(a, b, c) {
     if(a + b > c && b + c > a && c + a > b)
         return "y = 1"
     else 
         return "y = 0"
+}
+task25(3, 4, 5);
+//38
+function task38(a, b, c, d) {
+    if(d/c == c/b && c/b == b/a)
+        return true
+    else
+        return false
+} 
+//45
+function task45(x, y) {
+    if(x * x + y * y <= 1 && y - x > 0 && y + x > 0)
+        return 7 * x * x + 2
+    else 
+        return 0
 }
